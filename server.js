116550d28830df9d0ea46aa00842dc93398b9903
Alana Ragusa <alana.ragusarabello@gmail.com>
2022-02-07 22:25:39 -0300

    added server infos and forEach in index file

--- server.js
+++ server.js
@@ -4,7 +4,35 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res){
-    res.render("pages/index");
+    const items = [
+        {
+            title: "D", 
+            message: "esenvolver aplicações/serviços de forma fácil"
+        },
+        {
+            title: "E", 
+            message: "JS usa JavaScript para renderizar HTML"
+        },
+        {
+            title: "M", 
+            message: "uito fácil de usar"
+        },
+        {
+            title: "A", 
+            message: "morzinho"
+        },
+        {
+            title: "I", 
+            message: "nstall EJS"
+        },
+        {
+            title: "S", 
+            message: "intaxe simples"
+        }
+    ];
+    res.render("pages/index", {
+        qualities: items,
+    });
 })
 
 app.get("/sobre", function (req, res){
